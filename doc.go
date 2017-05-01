6bfa061e8c309147f6020030e80e61f0dee99fe8
nikepan <nikepan@yandex.ru>
2017-05-01 11:52:24 +0300

    fix docs

--- doc.go
+++ doc.go
@@ -1,32 +1,36 @@
 /*
-# ClickHouse-Bulk
+ClickHouse-Bulk
+
+Simple Yandex ClickHouse (https://clickhouse.yandex/) insert collector. It collect requests and send to ClickHouse servers.
 
 
-Simple [Yandex ClickHouse](https://clickhouse.yandex/) insert collector. It collect requests and send to ClickHouse servers.
+Features
 
-### Features
 - Group n requests and send to any of ClickHouse server
 - Sending collected data by interval
 - Tested with VALUES, TabSeparated formats
 - Supports many servers to send
 
+
 For example:
-```sql
+
 INSERT INTO table3 (c1, c2, c3) VALUES ('v1', 'v2', 'v3')
+
 INSERT INTO table3 (c1, c2, c3) VALUES ('v4', 'v5', 'v6')
-```
+
 sends as
-```sql
+
 INSERT INTO table3 (c1, c2, c3) VALUES ('v1', 'v2', 'v3')('v4', 'v5', 'v6')
-```
 
 
-### Options
+Options
+
 - -config - config file (json); default _config.json_
 
 
-### Configuration file
-```json{
+Configuration file
+
+{
   "listen": ":8124",
   "flush_count": 10000, // check by \n char
   "flush_interval": 1000, // milliseconds
@@ -38,16 +42,15 @@ INSERT INTO table3 (c1, c2, c3) VALUES ('v1', 'v2', 'v3')('v4', 'v5', 'v6')
     ]
   }
 }
-```
 
 
-### Quickstart
+Quickstart
 
 `./clickhouse-bulk`
 and send queries to :8124
 
 
-### Tips
+Tips
 
 For better performance words FORMAT and VALUES must be uppercase.
 
